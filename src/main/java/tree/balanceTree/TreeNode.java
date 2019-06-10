fdc5780a95f6847c762df80a0cc6a50ba29de981
kechenguang <kechenguang@xiaomi.com>
2019-06-10 14:17:29 +0800

    first commit

---
+++ src/main/java/tree/balanceTree/TreeNode.java
@@ -0,0 +1,50 @@
+package tree.balanceTree;
+
+public class TreeNode {
+
+    private int value;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+    private int leftHeight;
+    private int rightHeight;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public int getLeftHeight() {
+        return leftHeight;
+    }
+
+    public void setLeftHeight(int leftHeight) {
+        this.leftHeight = leftHeight;
+    }
+
+    public int getRightHeight() {
+        return rightHeight;
+    }
+
+    public void setRightHeight(int rightHeight) {
+        this.rightHeight = rightHeight;
+    }
+}
